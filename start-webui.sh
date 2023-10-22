a53fa689ebf0893b0ad7be41d6b7a3677a4c87e6
Elio Gonzalez <eliostanzione@hotmail.com>
2023-10-22 09:36:50 -0500

    modified sh bash file

--- start-webui.sh
+++ start-webui.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-source venv/bin/activate
+read -p "Path to the file(s) you want to process: " input_path
+if [ -z "$input_path" ]; then
+    echo "You did not provide a path. Exiting."
+    read -p "Press Enter to continue..."
+    exit
+fi
 
-PYTHON="venv/bin/python"
-echo "venv ${PYTHON}"
-echo ""
+activate_venv() {
+    PYTHON="./venv/bin/python"
+    echo "venv $PYTHON"
+}
 
-python app.py $*
+activate_venv
+launch() {
+    $PYTHON app.py --path "$input_path"
+    read -p "Press Enter to continue..."
+    exit
+}
 
-deactivate
-
+launch
